712b59188c206e70ae33f94867dac06dedce1d96
moeabdol <mohd.a.saed@gmail.com>
2018-01-11 00:10:19 +0300

    Transform props.ingredients object to flattened array

--- src/components/burger/burger.js
+++ src/components/burger/burger.js
@@ -4,12 +4,17 @@ import styles from './burger.css';
 import BurgerIngredient from './burger_ingredient/burger_ingredient';
 
 const burger = props => {
-  const ingredients = Object.keys(props.ingredients)
+  let ingredients = Object.keys(props.ingredients)
     .map(key => {
       return [...Array(props.ingredients[key])].map((_, i) => {
         return <BurgerIngredient key={key + i} type={key} />;
       });
-    });
+    })
+    .reduce((arr, el) => arr.concat(el), []);
+
+  if (ingredients.length === 0) {
+    ingredients = <p>Please start adding ingredients</p>;
+  }
 
   return (
     <div className={styles.burger}>
@@ -21,7 +26,7 @@ const burger = props => {
 };
 
 burger.propTypes = {
-  ingredients: PropTypes.Array
+  ingredients: PropTypes.object
 };
 
 export default burger;
