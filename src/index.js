c911365d1feb6f84b2148ecaa5f6aa176e72b7e3
moeabdol <mohd.a.saed@gmail.com>
2018-01-10 21:25:39 +0300

    Prepare project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './app';
 
-ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
