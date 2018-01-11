efbf0c904aef3e1c62680d06580449f7e3e43e56
moeabdol <mohd.a.saed@gmail.com>
2018-01-11 09:22:40 +0300

    Add build controls

--- src/components/burger/build_controls/build_controls.js
+++ src/components/burger/build_controls/build_controls.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import styles from './build_controls.css';
+import BuildControl from './build_control/build_control';
 
-const buildControls = props => (
-  <div>
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
+];
+
+const buildControls = () => (
+  <div className={styles.buildControls}>
+    {controls.map(ctrl => {
+      return <BuildControl key={ctrl.label} label={ctrl.label} />;
+    })}
   </div>
 );
 
--- src/containers/burger_builder/burger_builder.js
+++ src/containers/burger_builder/burger_builder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Burger from '../../components/burger/burger';
+import BuildControls from '../../components/burger/build_controls/build_controls';
 
 class BurgerBuilder extends Component {
   constructor(props) {
@@ -7,10 +8,10 @@ class BurgerBuilder extends Component {
 
     this.state = {
       ingredients: {
-        salad: 1,
-        bacon: 1,
-        cheese: 2,
-        meat: 2
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
       }
     };
   }
@@ -19,7 +20,7 @@ class BurgerBuilder extends Component {
     return (
       <React.Fragment>
         <Burger ingredients={this.state.ingredients} />
-        <div>Build Controls</div>
+        <BuildControls />
       </React.Fragment>
     );
   }
